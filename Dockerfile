9b16432a6b250e1196fe39947598d2ae8713c188
Blake Forrest <blakeforrest@Blakes-MacBook-Air.local>
2018-11-13 10:58:46 -0800

    Dockerization

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:10.9.0
+
+RUN mkdir -p /src/app
+
+WORKDIR /src/app
+
+COPY . /src/app
+
+RUN yarn install
+RUN yarn global add nodemon
+
+EXPOSE 3000
+
+CMD ["npm", "run", "start"]
--- server/index.js
+++ server/index.js
@@ -8,6 +8,26 @@ var port = 3000;
 app.use(bodyParser());
 app.use('/listings/:id', express.static(path.join(__dirname, '../public')));
 
+app.get('/listings/:id/description', (req, res) => {
+  const { id } = req.params;
+  res.redirect(`http://3.16.13.208/listings/${id}/description`);
+});
+
+app.get('/listings/:id/reservations', (req, res) => {
+  const { id } = req.params;
+  res.redirect(`http://13.57.206.173/listings/${id}/reservations`);
+});
+
+app.get('/listings/:id/reviews', (req, res) => {
+  const { id } = req.params;
+  res.redirect(`http://18.191.158.244/listings/${id}/reviews`);
+});
+
+app.get('/listings/:id/listings', (req, res) => {
+  const { id } = req.params;
+  res.redirect(`http://18.191.158.244/listings/${id}/listings`);
+});
+
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
